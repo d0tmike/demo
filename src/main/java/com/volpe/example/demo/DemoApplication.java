bb68c521c59c49d6e67a0c1c449647e421249588
d0tmike <d0tmike@users.noreply.github.com>
2021-07-02 23:13:03 -0400

    Update DemoApplication.java

--- src/main/java/com/volpe/example/demo/DemoApplication.java
+++ src/main/java/com/volpe/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.volpe.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// just a comment
+
 @SpringBootApplication
 public class DemoApplication {
 
